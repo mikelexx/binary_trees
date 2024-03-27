b7693eb61598c30cfa7f72d192351c89871620ed
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-03-27 19:20:51 +0300

    Return NULL if parent is null since there's no place to add child

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,6 +9,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *child = NULL;
 
+	if (parent == NULL)
+		return (NULL);
 	child = malloc(sizeof(binary_tree_t));
 	if (child == NULL)
 		return (NULL);
