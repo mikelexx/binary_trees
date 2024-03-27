42038b20e5d0eae99f1476ee57d02c3ea8f6522a
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-03-27 19:25:29 +0300

    left  right children of new node must be set to NULL

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,12 +9,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *child = NULL;
 
-	if (parent == NULL)
-		return (NULL);
 	child = malloc(sizeof(binary_tree_t));
 	if (child == NULL)
 		return (NULL);
 	child->n = value;
 	child->parent = parent;
+	child->left = NULL;
+	child->right = NULL;
 	return (child);
 }
